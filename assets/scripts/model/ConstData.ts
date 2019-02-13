c960b3de07568507a186a6a6933405bee10e7046
tornoto <LiuyuLite@163.com>
2019-02-13 20:44:45 +0800

    modify HUD_ROOT

---
+++ assets/scripts/model/ConstData.ts
@@ -0,0 +1,8 @@
+const {ccclass, property} = cc._decorator;
+
+export module SYS {
+    export var SH;      // window height
+    export var SW;      // window width
+    export var SH2;     // half window height
+    export var SW2;     // half window width
+}
--- assets/view/HUD_ROOT.ts
+++ assets/view/HUD_ROOT.ts
@@ -1,25 +1,42 @@
-const {ccclass, property} = cc._decorator;
+import { SYS } from "../scripts/model/ConstData";
+
+const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class ROOT_Layer extends cc.Component {
+export default class HUD_ROOT extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
-
-    data = [];
-
-    // LIFE-CYCLE CALLBACKS:
-
-    onLoad () {
+    onLoad() {
+        // add global variable
+        // (window as any).Mgr = Mgr;
         cc.game.addPersistRootNode(this.node);
+        ROOT_Layer = this;
     }
 
-    start () {
-        this.data.push("rootlayer");
+    start() {
+    }
+    _dt: number = 0;
+    update(dt) {
+        this._dt += dt;
+        if (this._dt >= 1) {
+            this._dt -= 1;
+            this.checkWinSize();
+        }
     }
 
-    // update (dt) {}
+    checkWinSize() {
+        if (cc.winSize.height != SYS.SH || Math.abs(cc.winSize.width - SYS.SW) > 1) {
+            SYS.SH = cc.winSize.height;
+            SYS.SW = cc.winSize.width;
+            SYS.SH2 = SYS.SH * 0.5;
+            SYS.SW2 = SYS.SW * 0.5;
+        }
+    }
+
+    onSizeChange() {
+        SYS.SH = cc.winSize.height;
+        SYS.SW = cc.winSize.width;
+        SYS.SH2 = SYS.SH * 0.5;
+        SYS.SW2 = SYS.SW * 0.5;
+    }
 }
+export var ROOT_Layer: HUD_ROOT;
