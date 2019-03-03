e26b8257ec64eb28b7253afd9d92942585da0de0
tornoto <LiuyuLite@163.com>
2019-03-03 10:45:34 +0800

    add scrolling background

---
+++ assets/view/scrollingbackground/ScrollingScene.ts
@@ -0,0 +1,37 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Node)
+    nBg1: cc.Node = null;
+    @property(cc.Node)
+    nBg2: cc.Node = null;
+
+    bgHeight: number = 0;
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.bgHeight = this.nBg1.height;
+    }
+
+    start () {
+        cc.log(`start ----\nbackground height: ${this.bgHeight}`);
+    }
+
+    update (dt) {
+        this.scrollingBg(dt);
+    }
+
+    scrollingBg(dt){
+        this.nBg1.setPosition(this.nBg1.position.x, this.nBg1.position.y - 1);
+        this.nBg2.setPosition(this.nBg2.position.x, this.nBg2.position.y - 1);
+        if(this.nBg1.position.y < -this.bgHeight){
+            this.nBg1.setPosition(this.nBg2.position.x, this.nBg2.position.y+512);
+        }
+        if(this.nBg2.position.y < -this.bgHeight){
+            this.nBg2.setPosition(this.nBg1.position.x, this.nBg1.position.y+512);
+        }
+    }
+}
